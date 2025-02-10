110f935fb6a19b539b0148ff514950babcb48df4
lizhiping <brigg2008@126.com>
2025-02-10 17:56:48 +0800

    行为人全景拼接

---
+++ build/CMakeFiles/MTPanoramicCamera.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/shenxikeji/stitingvideo/Pedestrian360/video_stitching/CameraParameter.cpp" "CMakeFiles/MTPanoramicCamera.dir/CameraParameter.cpp.o" "gcc" "CMakeFiles/MTPanoramicCamera.dir/CameraParameter.cpp.o.d"
+  "/home/shenxikeji/stitingvideo/Pedestrian360/video_stitching/MySeamFinder.cpp" "CMakeFiles/MTPanoramicCamera.dir/MySeamFinder.cpp.o" "gcc" "CMakeFiles/MTPanoramicCamera.dir/MySeamFinder.cpp.o.d"
+  "/home/shenxikeji/stitingvideo/Pedestrian360/video_stitching/Projector.cpp" "CMakeFiles/MTPanoramicCamera.dir/Projector.cpp.o" "gcc" "CMakeFiles/MTPanoramicCamera.dir/Projector.cpp.o.d"
+  "/home/shenxikeji/stitingvideo/Pedestrian360/video_stitching/Sticher.cpp" "CMakeFiles/MTPanoramicCamera.dir/Sticher.cpp.o" "gcc" "CMakeFiles/MTPanoramicCamera.dir/Sticher.cpp.o.d"
+  "/home/shenxikeji/stitingvideo/Pedestrian360/video_stitching/multiThread.cpp" "CMakeFiles/MTPanoramicCamera.dir/multiThread.cpp.o" "gcc" "CMakeFiles/MTPanoramicCamera.dir/multiThread.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MTPanoramicCamera.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MTPanoramicCamera.dir/CameraParameter.cpp.o"
+  "CMakeFiles/MTPanoramicCamera.dir/CameraParameter.cpp.o.d"
+  "CMakeFiles/MTPanoramicCamera.dir/MySeamFinder.cpp.o"
+  "CMakeFiles/MTPanoramicCamera.dir/MySeamFinder.cpp.o.d"
+  "CMakeFiles/MTPanoramicCamera.dir/Projector.cpp.o"
+  "CMakeFiles/MTPanoramicCamera.dir/Projector.cpp.o.d"
+  "CMakeFiles/MTPanoramicCamera.dir/Sticher.cpp.o"
+  "CMakeFiles/MTPanoramicCamera.dir/Sticher.cpp.o.d"
+  "CMakeFiles/MTPanoramicCamera.dir/multiThread.cpp.o"
+  "CMakeFiles/MTPanoramicCamera.dir/multiThread.cpp.o.d"
+  "MTPanoramicCamera"
+  "MTPanoramicCamera.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MTPanoramicCamera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
